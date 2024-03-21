d64617499da6d23acca336f1e0e7592e1f0e64c8
dermirez <dermirez@gmail.com>
2024-03-21 06:02:47 -0500

    New solution for excercice 30 mudule 7

--- C7 Arrays/ejercicios/30.js
+++ C7 Arrays/ejercicios/30.js
@@ -2,6 +2,21 @@ function encontrarElementoRepetido(numeros) {
   // La función recibe un argumento "numeros" que es un array de números.
   // Retorna el primer elemento repetido que se encuentre en el array.
   // Tu código:
+  
+  const testArray = [];
+  const repeatedArray = [];
+  numeros.forEach(num => {
+    if (testArray.includes(num)) {
+      repeatedArray.push(num);
+    } else {
+      testArray.push(num);
+    }
+  });
+  return repeatedArray.length === 0 ? undefined : repeatedArray[0]
+  
+
+  
+  /* 
   // Create an empty object to store the result
   const objNumeros = {};
   // Loop through each element in the array
@@ -28,11 +43,13 @@ function encontrarElementoRepetido(numeros) {
   // create minIndexDuplicateNum variable to store min element in array resultado, minIndexDuplicateNum is the index in original array numeros where we find a duplicate value. 
   var minIndexDuplicateNum = Math.min(...indexDuplicateNum);
   var firstDuplicateNumber = numeros[minIndexDuplicateNum] // create variable to store first repited element. 
-  return firstDuplicateNumber
+  return firstDuplicateNumber */
 }
 
 module.exports = encontrarElementoRepetido;
 
+// var resultado = encontrarElementoRepetido([2, 3, 4, 4, 2, 1]);
+// console.log(resultado);
 
  // Encontrar los elementos que se repiten y ordenarlos en un objeto. Los keys de dicho objeto deben ser los elementos repetidos del array numeros y los values correspondientes a cada key debe ser un array, los elementos de ese array son los índices de las posiciones en donde se encuntra ese elemento en el array numeros. 
   // ejemplo [2, 3, 3, 4, 2, 1], el obj = {2: [0, 4], 3: [1, 2], 4: [3], 1: [5]}
